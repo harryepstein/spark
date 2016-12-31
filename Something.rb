9ded7ae4e1ebd7c4f2738ca0a0348a4bf29a0b2a
Harry Epstein <harryeps@gmail.com>
2016-12-31 17:27:40 -0600

    some scripts from the first test of file saving

--- Something.rb
+++ Something.rb
@@ -18,11 +18,10 @@ links.each{|link|
 
  	 episodePage = Nokogiri::HTML(open("#{base_url}" + link['href']))
  		script = episodePage.css("div.scrolling-script-container").text
- 		puts script
- 		#File.write("#{link['href']}", script)#Write to individual files named and numbered according to their season and episode numbers.	 
- 	}
+ 		
+ 		File.write("#{link['href']}", script)#Write to individual files named and numbered according to their season and episode numbers.	 
+ }
 
 # ONCE IT FINDS THE SPECIFIC EPISODE PAGE RUN THIS SCRIPT TO OBTAIN THE SCRIPT TEXT
 # script = episodePage.css("div.scrolling-script-container").text
-
-
+	
